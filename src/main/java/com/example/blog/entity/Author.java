d082164386097b29a0c29ddcd3715b7cab397fd8
Max Legelbakh <maximusfk@gmail.com>
2017-10-09 10:57:09 +0300

    Created 'Author' class

---
+++ src/main/java/com/example/blog/entity/Author.java
@@ -0,0 +1,55 @@
+package com.example.blog.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Author")
+public class Author {
+	
+	@Id @GeneratedValue
+	@Column(name = "id")
+	private Integer id;
+	
+	@Column(name = "name")
+	private String name;
+	
+	@Column(name = "email")
+	private String email;
+	
+	public Author() {}
+
+	public Author(Integer id, String name, String email) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
