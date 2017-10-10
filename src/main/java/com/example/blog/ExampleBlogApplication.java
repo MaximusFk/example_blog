9d59059a8a1bf7d7602d7d70f7a3822c89da79cc
Max Legelbakh <maximusfk@gmail.com>
2017-10-10 22:30:49 +0300

    Added spring beans config for Hibernate

--- src/main/java/com/example/blog/ExampleBlogApplication.java
+++ src/main/java/com/example/blog/ExampleBlogApplication.java
@@ -2,8 +2,10 @@ package com.example.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:hibernate_bean_config.xml")
 public class ExampleBlogApplication {
 
 	public static void main(String[] args) {
