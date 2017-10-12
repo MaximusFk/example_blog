b0cbc5e16bf9b459fc666b3c4bec5be175f6f704
Max Legelbakh <maximusfk@gmail.com>
2017-10-12 16:22:16 +0300

    Added unit tests for 'AuthorHibernateDAOService' and 'ArticleHibernateDAOService'

---
+++ src/test/java/com/example/blog/unit/ArticleHibernateDAOServiceTests.java
@@ -0,0 +1,65 @@
+package com.example.blog.unit;
+
+import static org.junit.Assert.*;
+
+import java.sql.Date;
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.example.blog.database.ArticleHibernateDAOService;
+import com.example.blog.entity.Article;
+import com.example.blog.entity.Author;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration("classpath:hibernate_bean_test_config.xml")
+@Transactional
+public class ArticleHibernateDAOServiceTests {
+
+	@Autowired
+	ArticleHibernateDAOService articleDAOService;
+	
+	@Test
+	public void testSelectAll() {
+		List<Article> articles = articleDAOService.selectAll();
+		assertTrue(articles.size() > 0);
+	}
+
+	@Test
+	public void testSelect() {
+		Article article = articleDAOService.select(1);
+		assertNotNull(article);
+	}
+
+	@Test
+	public void testCreate() {
+		Author author = new Author(1, "Test name", "Test email");
+		Article article = new Article(15, author, "Test title", "Test label", "Test text", new Date(System.currentTimeMillis()));
+		Integer created_id = articleDAOService.create(article);
+		assertNotNull(created_id);
+	}
+
+	@Test
+	public void testUpdate() {
+		Article article = articleDAOService.select(1);
+		article.setTitle("Changet test title");
+		Integer id = articleDAOService.update(article);
+		assertNotNull(id);
+	}
+
+	@Test
+	public void testDelete() {
+		Boolean deleted = articleDAOService.delete(1);
+		assertNotNull(deleted);
+		assertTrue(deleted);
+	}
+
+}
---
+++ src/test/java/com/example/blog/unit/AuthorHibertnateDAOServiceTests.java
@@ -0,0 +1,62 @@
+package com.example.blog.unit;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.example.blog.database.AuthorHibernateDAOService;
+import com.example.blog.entity.Author;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration("classpath:hibernate_bean_test_config.xml")
+@Transactional
+public class AuthorHibertnateDAOServiceTests {
+
+	@Autowired
+	AuthorHibernateDAOService authorDAOService;
+	
+	@Test
+	public void testSelectAll() {
+		List<Author> authors = authorDAOService.selectAll();
+		assertTrue(authors.size() > 0);
+	}
+
+	@Test
+	public void testSelect() {
+		Author author = authorDAOService.select(1);
+		assertNotNull(author);
+	}
+
+	@Test
+	public void testCreate() {
+		Author author = new Author(15, "Test name", "test@email.com");
+		Integer created_id = authorDAOService.create(author);
+		assertNotNull(created_id);
+	}
+
+	@Test
+	public void testUpdate() {
+		Author author = authorDAOService.select(1);
+		author.setName("Changed test name");
+		Integer id = authorDAOService.update(author);
+		assertNotNull(id);
+	}
+
+	@Test
+	public void testDelete() {
+		Boolean deleted = authorDAOService.delete(1);
+		assertNotNull(deleted);
+		assertTrue(deleted);
+	}
+
+}
